012ae93b0eaedab8ac5587481fc9368c486268f1
sinepolskiyihor <sinepolskiyihor@gmail.com>
2019-12-25 02:54:16 +0200

    department #4

--- department.php
+++ department.php
@@ -75,7 +75,7 @@
   for ($i = 0; $i < count($rows); $i++){
 ?>
 <tr>
-  <td><select class="act" data-id="<?php $rows[$i]['id']; ?>"><option value="ACT">--Дії--</option><option value="UPDATE">Редагувати</option><option value="DELETE">Видалити</option></select></td>
+  <td><select class="act" data-id="<?php echo $rows[$i]['id']; ?>"><option value="ACT">--Дії--</option><option value="UPDATE">Редагувати</option><option value="DELETE">Видалити</option></select></td>
   <td attr="id"><?php echo  $rows[$i]['id'];?></td>
   <td attr="name"><?php echo  $rows[$i]['name'];?></td>
 </tr>
@@ -85,12 +85,12 @@
   </tbody>
   </table>
   <script>
-    var doms = document.querySelectorAll("SELECT.ACT");
+    var doms = document.querySelectorAll("select.act");
     for (var i = 0; i < doms.length; i++){
       doms[i].onchange = function(){
         var id = this.getAttribute("data-id");
         if(this.value == "UPDATE"){
-          var tds = this.parentNode.parentNode.querySelectorAll("TD");
+          var tds = this.parentNode.parentNode.querySelectorAll("td");
           for (var j = 1; j < tds.length; j++){
             var attr = tds[j].getAttribute("attr");
             if (attr == "id"){
@@ -105,6 +105,17 @@
         var butt = document.createElement('BUTTON');
         butt.setAttribute("data-id",id);
         butt.innerText = "зберегти";
+        butt.onclick = function(){
+          var id = this.getAttribute('data-id');
+          var inputs = this.parentNode.parentNode.querySelectorAll('input[data-id="'+id+'"]');
+          var url_params = "?id="+id;
+          for (var k = 0; k < inputs.length; k++){
+            var param = inputs[k].getAttribute('data-attr');
+            url_params += "&"+param+"="+encodeURIComponent(inputs[k].value);
+          }
+          doument.location = document.URL + url_params;
+          return false;
+        };
         parent.appendChild(butt);
         this.parentNode.removeChild(this);
       };
